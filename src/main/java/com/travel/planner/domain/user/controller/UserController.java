d2a01ebd311cc5d2d8725ef83a4764b7c784f9df
juyoung <wanke94@naver.com>
2026-01-30 12:05:59 +0900

    사용자 정보 가져오는 기능 추가

--- src/main/java/com/travel/planner/domain/user/controller/UserController.java
+++ src/main/java/com/travel/planner/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.travel.planner.domain.user.controller;
 
+import com.travel.planner.domain.user.dto.UserInfoResponse;
 import com.travel.planner.domain.user.service.UserService;
 import com.travel.planner.global.common.dto.BaseResponse;
 import com.travel.planner.global.exception.BusinessException;
@@ -18,6 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @Operation(summary = "현재 로그인한 사용자 정보 조회", description = "JWT 토큰을 통해 현재 로그인한 사용자의 정보를 반환합니다.")
+    @GetMapping("/info")
+    public ResponseEntity<BaseResponse<UserInfoResponse>> getCurrentUser() {
+        UserInfoResponse userInfo = userService.getCurrentUserInfo();
+        return ResponseEntity.ok(BaseResponse.success(userInfo));
+    }
 
     @Operation(summary = "성공 예제", description = "성공 예제")
     @GetMapping("/success")
